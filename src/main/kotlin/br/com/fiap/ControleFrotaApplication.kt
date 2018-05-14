d27486206e592f9f843974c85cf9367379c9685f
Marcelo Cerqueira <jmarcelo.cjr@gmail.com>
2018-05-13 21:23:22 -0300

    main application | properties | calculate cost

---
+++ src/main/kotlin/br/com/fiap/ControleFrotaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fiap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControleFrotaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ControleFrotaApplication>(*args)
+}
--- src/main/kotlin/br/com/fiap/controller/TripController.kt
+++ src/main/kotlin/br/com/fiap/controller/TripController.kt
@@ -2,7 +2,12 @@ package br.com.fiap.controller
 
 import br.com.fiap.entity.Car
 import br.com.fiap.entity.User
+import org.json.JSONObject
+import org.springframework.http.HttpEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.RestTemplate
+import java.io.FileInputStream
+import java.util.*
 
 @RestController
 @RequestMapping("/trip")
@@ -10,13 +15,42 @@ class TripController {
 
     @GetMapping("/cost")
     fun cost(
-        @RequestParam("fromLatitude") fromLatitude: Double,
-        @RequestParam("fromLongitude") fromLongitude: Double,
-        @RequestParam("toLatitude") toLatitude: Double,
-        @RequestParam("toLongitude") toLongitude: Double
+        @RequestParam("fromLatitude") fromLatitude: String,
+        @RequestParam("fromLongitude") fromLongitude: String,
+        @RequestParam("toLatitude") toLatitude: String,
+        @RequestParam("toLongitude") toLongitude: String
     ) {
         val valueKilometer = 2.00
         val valueMinute = 1.00
+
+        val url = "https://maps.googleapis.com/maps/api/directions/json?" +
+            "origin="+fromLatitude+","+fromLongitude+
+            "&destination="+toLatitude+","+toLongitude+"&mode=driving&units=metric&key="
+
+        var properties = Properties()
+        try{
+            val input = FileInputStream("application.properties")
+            properties.load(input)
+        }catch(e: Exception) {
+            System.out.println("Error getting property")
+        }
+
+        var restTemplate = RestTemplate()
+
+        val response = restTemplate.getForObject(url + properties.getProperty("api.google.secret.key"), String::class.java)
+
+        var jsonObject  = JSONObject(response)
+
+        val leg = jsonObject.getJSONObject("routes").getJSONArray("legs").getJSONObject(0)
+
+        val distanceKm = leg.getJSONObject("distance").get("value").toString().toInt() / 1000
+        val durationMinutes = leg.getJSONObject("duration").get("value").toString().toInt() / 60
+
+        val responseJson = JSONObject()
+
+        var cost = (valueKilometer * distanceKm) + (valueMinute * durationMinutes)
+
+        responseJson.append("value", cost)
     }
 
     @PostMapping("/request")
